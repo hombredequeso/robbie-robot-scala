ab123e3bd26f9fed4b47597a29c443bf811e6a09
mcheeseman <mcheeseman@users.noreply.github.com>
2017-01-14 10:18:17 +1100

    Function parameterization
    Make board play varariables parameters, (boardCount, numberOfTurnsPerBoard).
    
    Move functions within findOptionalStrategy out into object namespace

--- src/main/scala-2.12/GeneticAlgorithm.scala
+++ src/main/scala-2.12/GeneticAlgorithm.scala
@@ -5,40 +5,54 @@ import com.hombredequeso.robbierobot.Strat.Strategy
 
 object GeneticAlgorithm {
 
+  def getStrategyFitness
+  (strategy: Strategy)
+  (boardCount: Int, numberOfTurnsPerBoard: Int)
+  : Int = {
+    val boards = (1 to boardCount).map(x => Board.createRandomBoard(100, 100, 0.5f))
+    val fitness = boards.map(b =>
+      Play.execute(
+        Play.State(b, Play.initialRobotPosition),
+        strategy,
+        numberOfTurnsPerBoard))
+    fitness.sum
+  }
 
-  def findOptimalStrategy(iterations: Int, populationSize: Int): Strategy = {
+  def iterate
+  (population: Vector[Strategy])
+  (boardCount: Int, numberOfTurnsPerBoard: Int)
+  : Vector[Strategy] = {
+    val members =
+      population.map(
+        s => Member(s, getStrategyFitness(s)(boardCount, numberOfTurnsPerBoard)))
+    val newPopulation = Evolve.evolve(members)
+    newPopulation
+  }
+
+
+  def goForIt
+  (population: Vector[Strategy], iterationsRemaining: Int )
+  (boardCount: Int, numberOfTurnsPerBoard: Int)
+  : Vector[Strategy] = {
+    if (iterationsRemaining == 0)
+      population
+    else {
+      val newStrategies = iterate(population)(boardCount, numberOfTurnsPerBoard)
+      goForIt(newStrategies, iterationsRemaining - 1)(boardCount, numberOfTurnsPerBoard)
+    }
+  }
+
+  def findOptimalStrategy
+  (iterations: Int, populationSize: Int)
+  (boardCount: Int, numberOfTurnsPerBoard: Int)
+  : Strategy = {
     val initialPopulation = createInitialPopulation(populationSize)
 
-    def getStrategyFitness(strategy: Strategy): Int = {
-      val boardCount = 100
-      val numberOfTurnsPerBoard = 200
-      val boards = (1 to boardCount).map(x => Board.createRandomBoard(100, 100, 0.5f))
-      val fitness = boards.map(b =>
-        Play.execute(
-          Play.State(b, Play.initialRobotPosition),
-          strategy,
-          numberOfTurnsPerBoard))
-      fitness.sum
-    }
-
-    def iterate(population: Vector[Strategy]): Vector[Strategy] = {
-      val members = population.map(s => Member(s, getStrategyFitness(s)))
-      val newPopulation = Evolve.evolve(members)
-      newPopulation
-    }
-
-
-    def goForIt(population: Vector[Strategy], iterationsRemaining: Int ): Vector[Strategy] = {
-      if (iterationsRemaining == 0)
-        population
-      else {
-        val newStrategies = iterate(population)
-        goForIt(newStrategies, iterationsRemaining - 1)
-      }
-    }
-
-    val finalResult = goForIt(initialPopulation, iterations)
-    val finalResultWithFitness = finalResult.map(s => Member(s, getStrategyFitness(s)))
+    val finalResult =
+      goForIt(initialPopulation, iterations)(boardCount, numberOfTurnsPerBoard)
+    val finalResultWithFitness =
+      finalResult
+        .map(s => Member(s, getStrategyFitness(s)(boardCount, numberOfTurnsPerBoard)))
     val bestStrategy = finalResultWithFitness.sortBy(x => x.fitness).last
     bestStrategy.strategy
   }
--- src/test/scala-2.12/GeneticAlgorithmSpec.scala
+++ src/test/scala-2.12/GeneticAlgorithmSpec.scala
@@ -7,10 +7,14 @@ class GeneticAlgorithmSpec extends FunSpec with Matchers {
 
   describe("findOptimalStrategy") {
     var iterationCount = 3;
-    val populationSize = 100
+    val populationSize = 100;
+    var boardCount = 10;
+    var numberOfTurnsPerBoard = 50;
 
     it(s"does not blow up in a puff of smoke (iterationCount = ${iterationCount})") {
-      GeneticAlgorithm.findOptimalStrategy(iterationCount, populationSize)
+      GeneticAlgorithm.findOptimalStrategy(
+        iterationCount, populationSize)(
+        boardCount, numberOfTurnsPerBoard)
     }
   }
 }
